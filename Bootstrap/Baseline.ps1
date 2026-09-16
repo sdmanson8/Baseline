106a4c4132b7ba1594d6d9c7a855bd3e81a5919f
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2026-09-16 08:52:50 +0200

    Bump version to 1.1.0 and update release notes
    Updated version number and added release date for minor fixes.

--- Bootstrap/Baseline.ps1
+++ Bootstrap/Baseline.ps1
@@ -3,11 +3,12 @@
 	WPF GUI for Windows 10 & Windows 11 fine-tuning and automating the routine tasks
 
     .VERSION
-    1.0.0
+    1.1.0
 
     .DATE
     21.03.2026 - Initial Release
     25.05.2026 - Major upgrades to GUI
+	16.09.2026 - Minor bugs and fixes
 
 	.AUTHOR
 	sdmanson8 - Copyright (c) 2026
